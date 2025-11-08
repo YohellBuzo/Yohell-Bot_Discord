72be005f616c9252cde8225675b04ce8452be1ef
YohellBuzo <yohellbuzo7@gmail.com>
2025-11-08 13:35:15 -0600

    Error correction

--- src/services/wisdom_service.ts
+++ src/services/wisdom_service.ts
@@ -19,15 +19,26 @@ export async function insertWisdom(
   const query = `INSERT INTO wisdom (title, paragraph, userId) 
      VALUES ($1, $2, $3) RETURNING title, paragraph, userId`;
 
-  const result = db.query(query, [title, paragraph, userid]);
+  const result = await db.query(query, [title, paragraph, userid]);
   return Wisdom.createFromObject((await result).rows[0]);
 }
 
 export async function getWisdomByTitle(title: string): Promise<Wisdom> {
   const query = `SELECT id, title, paragraph, userId, created_at FROM wisdom WHERE title = $1`;
 
-  const result = db.query(query, [title]);
+  const result = await db.query(query, [title]);
   return Wisdom.createFromObject((await result).rows[0]);
+}
+
+export async function getWisdomByUser(userId: string): Promise<Wisdom[]> {
+  const query = `SELECT * FROM wisdom WHERE userid = $1 ORDER BY id`;
+  const result = await db.query(query, [userId]);
+
+  if (!result.rows || result.rows.length === 0) {
+    return [];
+  }
+
+  return Wisdom.createFromJSON(result.rows);
 }
 
 export async function getLastWisdom(): Promise<Wisdom> {
