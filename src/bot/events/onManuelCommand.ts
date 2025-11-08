a04ff50c7252af88667e38b0baa6aa2d976e0bd3
YohellBuzo <yohellbuzo7@gmail.com>
2025-11-08 16:33:49 -0600

    Added !manuel get functionality to see an user wisdom

--- src/bot/events/onManuelCommand.ts
+++ src/bot/events/onManuelCommand.ts
@@ -1,12 +1,20 @@
-import { EmbedBuilder, PermissionsBitField } from "discord.js";
+import {
+  ChannelType,
+  EmbedBuilder,
+  Message,
+  PermissionsBitField,
+  User,
+} from "discord.js";
 import { client } from "../client";
 import {
   getAllWisdoms,
   getLastWisdom,
   getWisdomByTitle,
+  getWisdomByUser,
   insertWisdom,
 } from "../../services/wisdom_service";
 import { ValidationService } from "../../utils/validation_service";
+import { Wisdom } from "../../classes/wisdom_class";
 
 // const generateCode = (length: number = 3): string => {
 //   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -28,30 +36,42 @@ const getRandomImage = () => {
   return imageURL;
 };
 
-const sendRandomWisdom = async (message: any) => {
+const createEmbed = (user: User, wisdom: Wisdom, message: Message) => {
+  const embed = new EmbedBuilder();
+  embed.setTitle(wisdom.title);
+  embed.setDescription(wisdom.paragraph);
+  embed.setColor("#ec53a0");
+  embed.setImage(getRandomImage());
+  embed.setThumbnail(user.displayAvatarURL());
+  embed.setFooter({
+    text: `Sabiduría de ${user.username}`,
+  });
+
+  if (!ValidationService.hasValidEmbed(embed)) {
+    message.reply("No se pudo crear el mensaje correctamente.");
+    return null;
+  }
+
+  return embed;
+};
+
+const sendRandomWisdom = async (message: Message) => {
+  if (message.channel.type !== ChannelType.GuildText) return;
+  if (message.author.bot) return;
+
   try {
     const wisdoms = await getAllWisdoms();
     const random = wisdoms[Math.floor(Math.random() * wisdoms.length)];
-
     const user = await client.users.fetch(random.userid);
 
-    const embed = new EmbedBuilder();
-    embed.setTitle(random.title);
-    embed.setDescription(random.paragraph);
-    embed.setColor("#ec53a0");
-    embed.setImage(getRandomImage());
-    embed.setThumbnail(user.displayAvatarURL());
-    embed.setFooter({
-      text: `Sabiduría de ${user.username}`,
-    });
+    const embed = createEmbed(user, random, message);
+    if (!embed) return;
 
-    if (!ValidationService.hasValidEmbed(embed)) {
-      return message.reply("No se pudo crear el mensaje correctamente.");
+    if (message.channel.isTextBased()) {
+      await message.channel.send({
+        embeds: [embed],
+      });
     }
-
-    await message.channel.send({
-      embeds: [embed],
-    });
   } catch (error: any) {
     console.error("Error al enviar el mensaje:", error);
     try {
@@ -64,7 +84,8 @@ const sendRandomWisdom = async (message: any) => {
   }
 };
 
-const sendWisdom = async (message: any, title: string) => {
+const sendWisdom = async (message: Message, title: string) => {
+  if (message.channel.type !== ChannelType.GuildText) return;
   if (message.author.bot) return;
 
   if (title.length < 2) {
@@ -79,41 +100,33 @@ const sendWisdom = async (message: any, title: string) => {
 
     const user = await client.users.fetch(result.userid);
 
-    const embed = new EmbedBuilder();
-    embed.setTitle(result.title);
-    embed.setDescription(result.paragraph);
-    embed.setColor("#ec53a0");
-    embed.setImage(getRandomImage());
-    embed.setThumbnail(user.displayAvatarURL());
-    embed.setFooter({
-      text: `Sabiduría de ${user.username}`,
-    });
+    const embed = createEmbed(user, result, message);
+    if (!embed) return;
 
-    if (!ValidationService.hasValidEmbed(embed)) {
-      return message.reply("No se pudo crear el mensaje correctamente.");
+    if (message.channel.isTextBased()) {
+      await message.channel.send({
+        embeds: [embed],
+      });
     }
-
-    await message.channel.send({
-      embeds: [embed],
-    });
   } catch (error) {
     console.error("❌ Error al guardar sabiduría:", error);
     await message.reply("Valió berenjena la busqueda.");
   }
 };
 
-const addWisdom = async (message: any, paragraph: string) => {
+const addWisdom = async (message: Message, paragraph: string) => {
+  if (message.channel.type !== ChannelType.GuildText) return;
   if (message.author.bot) return;
 
   if (paragraph.length === 0) {
     return message.reply(
-      'Leer es lava, el formarto es.\n`!manuel add "Parrafo"` las comillas no están de adorno.'
+      'Leer es lava, el formato es.\n`!manuel add "Parrafo"` las comillas no están de adorno.'
     );
   }
 
   if (!paragraph.startsWith('"') || !paragraph.endsWith('"')) {
     return message.reply(
-      'Leer es lava, el formarto es.\n`!manuel add "Parrafo"` las comillas no están de adorno.'
+      'Leer es lava, el formato es.\n`!manuel add "Parrafo"` las comillas no están de adorno.'
     );
   }
 
@@ -138,7 +151,7 @@ const addWisdom = async (message: any, paragraph: string) => {
 
     if (cleanParagraph.length === 0) {
       return message.reply(
-        'Leer es lava, el formarto es.\n`!manuel add "Parrafo"` las comillas no están de adorno.'
+        'Leer es lava, el formato es.\n`!manuel add "Parrafo"` las comillas no están de adorno.'
       );
     }
 
@@ -160,9 +173,11 @@ const addWisdom = async (message: any, paragraph: string) => {
       { name: "Autor", value: `<${user.username}>`, inline: true },
       { name: "Título", value: `<${result.title}>`, inline: true },
     ]);
-    embed.setFooter({
-      text: `Sabiduría de ${user.username}`,
-    });
+    if (message.channel.isTextBased()) {
+      await message.channel.send({
+        embeds: [embed],
+      });
+    }
 
     await message.channel.send({ embeds: [embed] });
   } catch (error) {
@@ -171,7 +186,69 @@ const addWisdom = async (message: any, paragraph: string) => {
   }
 };
 
+export const sendWisdomByUser = async (message: Message) => {
+  if (message.channel.type !== ChannelType.GuildText) return;
+  if (message.author.bot) return;
+
+  const user = message.mentions.users.first();
+  
+  if (!user) {
+    return message.reply(
+      `Leer es lava, el formato es.\n!manuel get @usuario, el @ no está de adorno.`
+    );
+  }
+
+  const wisdomList = await getWisdomByUser(user!.id);
+  let index = 0;
+
+  const embed = createEmbed(user!, wisdomList[index], message);
+  if (!embed) return;
+
+  const sentMessage = await message.channel.send({ embeds: [embed] });
+  await sentMessage.react("⬅️");
+  await sentMessage.react("➡️");
+
+  const filter = (reaction: any, user: User) =>
+    ["⬅️", "➡️"].includes(reaction.emoji.name) && user.id === message.author.id;
+
+  const collector = sentMessage.createReactionCollector({
+    filter,
+    time: 120000,
+  });
+
+  collector.on("collect", async (reaction, user) => {
+    if (reaction.emoji.name === "➡️") {
+      index = (index + 1) % wisdomList.length;
+    } else if (reaction.emoji.name === "⬅️") {
+      index = (index - 1 + wisdomList.length) % wisdomList.length;
+    }
+
+    const newEmbed = createEmbed(user, wisdomList[index], message);
+    if (!newEmbed) return;
+
+    await sentMessage.edit({ embeds: [newEmbed] });
+
+    try {
+      await reaction.users.remove(user.id);
+    } catch (error: any) {
+      if (error.code === 50013) {
+        console.warn(
+          "Sin permidos no hay servicios. Se ocupa el permiso de eliminar reacciones."
+        );
+      } else {
+        console.error("❌ Error al eliminar la reacción:", error);
+      }
+    }
+  });
+
+  collector.on("end", () => {
+    sentMessage.reactions.removeAll().catch(() => {});
+  });
+};
+
 export const onManuelCommand = client.on("messageCreate", async (message) => {
+  if (message.channel.type !== ChannelType.GuildText) return;
+
   if (!ValidationService.isCommand(message, "!manuel")) return;
   if (
     !(await ValidationService.hasBotPermission(
@@ -187,6 +264,8 @@ export const onManuelCommand = client.on("messageCreate", async (message) => {
     ))
   )
     return;
+
+  if (!message.guild) return;
 
   const command = message.content.trim();
   const commandParts = command.split(" ");
@@ -207,6 +286,11 @@ export const onManuelCommand = client.on("messageCreate", async (message) => {
   //Search command 1 argument [#number]
   if (commandParts[1].startsWith("#")) {
     await sendWisdom(message, commandParts[1] ?? "");
+    return;
+  }
+
+  if (commandParts[1] === "get") {
+    await sendWisdomByUser(message);
     return;
   }
 });
